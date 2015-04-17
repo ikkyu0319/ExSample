d3ac22f0eacee45aa28485151c1cf16f1385e457
ikkyu0319 <tom5953@163.com>
2015-04-17 16:20:41 +0800

    Merge branch 'master' of github.com:ikkyu0319/ExSample
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- ExLibrary/src/main/java/com/androidex/activity/ExActivity.java
+++ ExLibrary/src/main/java/com/androidex/activity/ExActivity.java
@@ -1,18 +1,8 @@
 package com.androidex.activity;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import com.androidex.R;
 
 public class ExActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ex);
-    }
 
 }
