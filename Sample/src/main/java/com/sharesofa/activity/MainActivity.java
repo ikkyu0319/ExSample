3e1846ab546ee9c0d4a29aac51a1002974acd1ee
tom <wei.wang.a@qyer.com>
2016-05-18 15:45:15 +0800

    some code

--- Sample/src/main/java/com/sharesofa/activity/MainActivity.java
+++ Sample/src/main/java/com/sharesofa/activity/MainActivity.java
@@ -1,7 +1,6 @@
 package com.sharesofa.activity;
 
 import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ListView;
@@ -9,9 +8,7 @@ import android.widget.ListView;
 import com.androidex.adapter.singletype.ExCommonLvAdapter;
 import com.androidex.adapter.singletype.ExCommonRcvAdapter;
 import com.androidex.adapter.singletype.ExViewHolder;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
+import com.sharesofa.R;
 import com.sharesofa.bean.Users;
 
 import java.util.ArrayList;
@@ -19,7 +16,6 @@ import java.util.List;
 
 
 public class MainActivity extends Activity {
-
 
 
     @Override
@@ -49,9 +45,6 @@ public class MainActivity extends Activity {
             });
 
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
@@ -59,39 +52,11 @@ public class MainActivity extends Activity {
     public void onStart() {
         super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.sharesofa.activity/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
     }
 
     @Override
     public void onStop() {
         super.onStop();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.sharesofa.activity/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 }
